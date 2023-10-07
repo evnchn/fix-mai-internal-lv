d17478c206790fad5c0d83d6b101058bf18505b7
unknown <evanchan040511@gmail.com>
2023-10-08 02:38:38 +0800

    include myjian.github.io domain for userscript fixer

--- fix-mai-internal-lv.userscript.js
+++ fix-mai-internal-lv.userscript.js
@@ -1,14 +1,17 @@
 // ==UserScript==
 // @name         fix-mai-internal-lv
 // @namespace    https://github.com/evnchn/fix-mai-internal-lv
-// @version      0.1
+// @version      0.2
 // @description  mai-tools broke because maidx_in_lv_festivalplus.js no longer available from upstream data source. This is a hotfix to put in the "magicVer20" required for function.
 // @match        https://maimaidx-eng.com/maimai-mobile/record/*
+// @match        https://myjian.github.io/mai-tools/rating-calculator/*
 // @downloadURL  https://raw.githubusercontent.com/evnchn/fix-mai-internal-lv/main/fix-mai-internal-lv.userscript.js
 // @updateURL    https://raw.githubusercontent.com/evnchn/fix-mai-internal-lv/main/fix-mai-internal-lv.userscript.js
 // ==/UserScript==
 
 // V0.1: emergency release
+
+// V0.2: include myjian.github.io domain
 
 (function () {
     'use strict';
@@ -31,8 +34,14 @@
             });
 
     }
-
-    document.querySelector("header").nextElementSibling.querySelectorAll('img.w_120')[3].style.filter = "invert()"
-    document.querySelector("header").nextElementSibling.querySelectorAll('img.w_120')[3].onclick = function () { fetchCustomData() }
+    if (location.href.indexOf("myjian") != -1) {
+        var button = document.createElement("button");
+        button.innerHTML = "HOTFIX mai-tools";
+        button.addEventListener("click", fetchCustomData);
+        document.body.prepend(button);
+    } else {
+        document.querySelector("header").nextElementSibling.querySelectorAll('img.w_120')[3].style.filter = "invert()"
+        document.querySelector("header").nextElementSibling.querySelectorAll('img.w_120')[3].onclick = function () { fetchCustomData() }
+    }
 
 })();
